a3397e35e38d386ac3d62f69132833bff16ae51b
Akshatha <54105823+Akshatha967@users.noreply.github.com>
2023-10-13 23:24:38 +0530

    Add files via upload

---
+++ Java_training/dayEight/DateTimeApi_dayEight/DateTimeApis.java
@@ -0,0 +1,14 @@
+package DateTimeApi_dayEight;
+
+import java.time.LocalDate;
+
+public class DateTimeApis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalDate lc = LocalDate.now();
+		System.out.println(lc.atStartOfDay());
+
+	}
+
+}
